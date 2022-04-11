3102a0c6aaa1c2f9380a7700d4da8dd09eeaa4ea
RoPlaceAPI <103414348+RoPlaceAPI@users.noreply.github.com>
2022-04-11 05:09:04 -0400

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -1,14 +1,22 @@
 --> Variables
+local console = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RobloxArchiver/Console/main/src/main.lua"))() 
 local CanvasPath = game:GetService("Workspace").Canvas
---> Console Library
-local console = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/RobloxArchiver/Console/main/src/main.lua"))()
+local rconsoleAssert = assert(rconsoleprint, "rconsole not supported")
+local LocalPlayer = game:GetService("Players").LocalPlayer
 
---> Main
-for index, block in pairs(CanvasPath:GetChildren()) do
-    if block:IsA("Part") then
-        console.log("Indexing: " ..index)
-        console.info("  Position: " .. block.Name) --> The name is the "pixel positon" not the actual position.
-        console.info("  Color: " .. tostring(block.Color))
+--> Console Log
+if getfenv().logToConsole == true then
+    if rconsoleAssert then
+        for index, block in pairs(CanvasPath:GetChildren()) do
+            if block:IsA("Part") then
+                console:Color("BLUE")
+                console.log("Indexing: " ..index)
+                console.info("  Position: " .. block.Name) --> The name is the "pixel positon" not the actual position.
+                console.info("  Color: " .. tostring(block.Color))
+            end
+        end
+    else
+        LocalPlayer:Kick("rconsoleprint not supported.")
     end
 end
 
@@ -17,11 +25,6 @@ console.log("Operation Complete!")
 
 --[[
 
-    Will make it export to CSV. 
-    With that it can easily be edited a bit to be changed to JSON. 
-
-    Or maybe I can make it export to JSON.
-
-    This is meantt to be like a... Testbench!
+Currently working on exports to file.
 
 ]]
